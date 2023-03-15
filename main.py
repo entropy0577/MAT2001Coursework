02a04d6a6c1f24c654423002ec29587049597e5e
Rhys Ilott <daricecake@protonmail.com>
2023-03-15 20:13:45 +0000

    started switch to generator functions

--- main.py
+++ main.py
@@ -12,9 +12,19 @@ def load_gs_img(path):
     return img_as_arr
 
 
-img_as_arr = load_gs_img('test.png')
+# Solves part 2. Uses list comprehension to create a new list
+def get_faces(hindex, vindex, img_as_arr):
+    yield (img_as_arr[(26 + i*123):(149 + (i*123)), j*127:(126 + j * 127)].flatten() for i in range(hindex)
+            for j in range(vindex))
 
-print(img_as_arr.shape)
+# Using list comprehension
+def variance_of_arr(array):
+    return np.asarray([np.linalg.norm(i - np.mean(i)) / len(array) for i in array])
 
-print(img_as_arr)
 
+
+test_as_arr = load_gs_img('test.png')
+faces_as_arr = load_gs_img('faces.png')
+face_list = get_faces(4, 6, faces_as_arr)
+
+print(face_list)
